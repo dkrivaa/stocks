24e74b7f1f060b2146ac7fc44407ce70bc48ce42
dkrivaa <dannykrivaa@hotmail.com>
2023-07-02 14:54:40 +0300

    Stock game

--- design/design.py
+++ design/design.py
@@ -10,6 +10,12 @@ def basic_setup():
     st.set_page_config(page_title='Nasdaq Stock Game', layout='wide')
     st.markdown(f'<span style="color: #18448c; font-size: 32px"><b>NASDAQ STOCK GAME</b></span>'
              , unsafe_allow_html=True)
+    with st.container:
+        cols = st.columns(8)
+        with cols[0]:
+            st.button('Start New Game', on_click=new_game)
+        with cols[1]:
+            st.button('Continue Game', on_click=continue_game)
     st.markdown('___')
 
 
@@ -23,12 +29,10 @@ def start_page():
             url = 'https://cdn1.iconfinder.com/data/icons/finance-251/64/56-64.png'
             st.image(images.get_image(url))
             st.write('')
-            st.button('Start New Game', on_click=new_game)
         with cols[1]:
             url = 'https://cdn0.iconfinder.com/data/icons/customicondesignoffice5/64/continue.png'
             st.image(images.get_image(url))
             st.write('')
-            st.button('Continue Game', on_click=continue_game)
 
 
 # New Game
