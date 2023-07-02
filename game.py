bacb9ab70923f214355286af1fc334a91ab2ce29
dkrivaa <dannykrivaa@hotmail.com>
2023-07-02 09:24:28 +0300

    Stock game

--- game.py
+++ game.py
@@ -2,8 +2,14 @@ import streamlit as st
 
 
 from design import design
+from stock_data import stocks
 
+
+# setting up basic setup
 design.basic_setup()
+
+st.write(stocks.get_data())
+
 
 
 
--- stock_data/stocks.py
+++ stock_data/stocks.py
@@ -1,12 +1,23 @@
 import requests
 import json
 
+
 # GETTING DATA FOR ALL STOCKS ON NASDAQ (latest close)
-url = 'https://stockanalysis.com/api/screener/s/f?m=marketCap&s=desc&c=no,s,n,marketCap,price,change,revenue,volume,industry,sector,revenueGrowth,netIncome,fcf,netCash&cn=0&f=exchange-is-NASDAQ&p=2&dd=true&i=allstocks'
-response = requests.get(url)
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Parse the JSON data
-    stock_data = json.loads(response.text)
-# Extract data and make dataframe
-nested_data = stock_data['data']['data']
+def get_data():
+    url = 'https://stockanalysis.com/api/screener/s/f?m=marketCap&s=desc&c=no,s,n,marketCap,price,change,revenue,volume,industry,sector,revenueGrowth,netIncome,fcf,netCash&cn=0&f=exchange-is-NASDAQ&p=2&dd=true&i=allstocks'
+    response = requests.get(url)
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON data
+        stock_data = json.loads(response.text)
+    # Extract data and make dataframe
+    nested_data = stock_data['data']['data']
+    return nested_data
+
+
+#Making dataframe with all stock data
+# df = pd.DataFrame(nested_data)
+# df = df.rename(columns={'s': 'symbol', 'n': 'company name', 'marketCap': 'market capital',
+#                         'price': 'stock price', 'change': 'percentage change',
+#                         'fcf': 'free cash flow', 'netCash': 'net chash / debt'})
+# df.drop('no', axis=1, inplace=True)
