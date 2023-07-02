dc0bca59dac63d457a9039987d97ad65fbcd27c5
dkrivaa <dannykrivaa@hotmail.com>
2023-07-02 10:52:02 +0300

    Stock game

--- design/design.py
+++ design/design.py
@@ -11,17 +11,17 @@ def basic_setup():
 
 
 def start_page():
-    cols = st.columns([1, 12])
+    cols = st.columns([1, 8])
     with cols[0]:
         url = 'https://cdn4.iconfinder.com/data/icons/success-filloutline/64/chart-analytics-stocks-increasing-growth-64.png'
         st.image(images.get_image(url))
         st.write('')
+        st.markdown(f'<span style="color: #18448c; font-size: 24px"><b>Start New Game</b></span>',
+                    unsafe_allow_html=True)
+    with cols[1]:
         url = 'https://cdn0.iconfinder.com/data/icons/customicondesignoffice5/64/continue.png'
         st.image(images.get_image(url))
-    with cols[1]:
-        st.markdown(f'<span style="color: #18448c; font-size: 32px"><b>Start New Game</b></span>',
-                    unsafe_allow_html=True)
         st.write('')
-        st.markdown(f'<span style="color: #18448c; font-size: 32px"><b>Continue Game</b></span>',
+        st.markdown(f'<span style="color: #18448c; font-size: 24px"><b>Continue Game</b></span>',
                     unsafe_allow_html=True)
 
