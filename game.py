1953175677813e80278c4747e962cc916177e1bd
dkrivaa <dannykrivaa@hotmail.com>
2023-07-02 16:50:30 +0300

    Stock game

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from design import design
 from stock_data import stocks
 from repo import repo_files
@@ -5,8 +6,11 @@ from repo import repo_files
 # setting up basic page setup
 design.basic_setup()
 
+st.write(stocks.get_data()
+)
+
 # Save base df
-repo_files.save_new_file(stocks.get_data(), 'test')
+# repo_files.save_new_file(stocks.get_data(), 'test')
 
 
 
