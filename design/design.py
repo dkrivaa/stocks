8d5192012fb0a01ed9dda5a8a7f6de029ea93cfc
dkrivaa <dannykrivaa@hotmail.com>
2023-07-02 12:25:25 +0300

    Stock game

--- design/design.py
+++ design/design.py
@@ -4,7 +4,7 @@ from design import images
 
 # BASIC SETUP OF WEB PAGE
 def basic_setup():
-    st.set_page_config(page_title='Nasdaq Stock Game', layout='wide')
+    st.set_page_config(page_title='Nasdaq Stock Game')
     st.markdown(f'<span style="color: #18448c; font-size: x-large"><b>NASDAQ STOCK GAME</b></span>'
              , unsafe_allow_html=True)
     st.markdown('___')
