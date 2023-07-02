2958f9eeb9e425ae1ee87e58e4a07814294c0683
dkrivaa <dannykrivaa@hotmail.com>
2023-07-02 09:41:20 +0300

    Stock game

--- design/design.py
+++ design/design.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 # BASIC SETUP OF WEB PAGE
 def basic_setup():
-    st.set_page_config(page_title='Nasdaq Stock Game', layout='wide')
+    st.set_page_config(page_title='Nasdaq Stock Game')
     st.markdown(f'<span style="color: #18448c; font-size: x-large"><b>NASDAQ STOCK GAME</b></span>'
              , unsafe_allow_html=True)
     st.markdown('___')
