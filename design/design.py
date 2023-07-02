bcb527c91396f602e57f3266f31cfd516034fa04
dkrivaa <dannykrivaa@hotmail.com>
2023-07-02 15:00:23 +0300

    Stock game

--- design/design.py
+++ design/design.py
@@ -9,7 +9,7 @@ def basic_setup():
     st.set_page_config(page_title='Nasdaq Stock Game', layout='wide')
     st.markdown(f'<span style="color: #18448c; font-size: 32px"><b>NASDAQ STOCK GAME</b></span>'
              , unsafe_allow_html=True)
-    with st.container:
+    with st.container():
         cols = st.columns(8)
         with cols[0]:
             st.button('Start New Game', on_click=new_game)
