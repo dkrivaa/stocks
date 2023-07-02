d827254e52bb0aacd7c80c3177903a60e88ed80f
dkrivaa <dannykrivaa@hotmail.com>
2023-07-02 13:17:56 +0300

    Stock game

--- design/design.py
+++ design/design.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 from design import images
 
+
+start = True
+
 # BASIC SETUP OF WEB PAGE
 def basic_setup():
     st.set_page_config(page_title='Nasdaq Stock Game', layout='wide')
@@ -12,17 +15,18 @@ def basic_setup():
 
 # Start page
 def start_page():
-    cols = st.columns([1, 2])
-    with cols[0]:
-        url = 'https://cdn1.iconfinder.com/data/icons/finance-251/64/56-64.png'
-        st.image(images.get_image(url))
-        st.write('')
-        st.button('Start New Game', on_click=new_game)
-    with cols[1]:
-        url = 'https://cdn0.iconfinder.com/data/icons/customicondesignoffice5/64/continue.png'
-        st.image(images.get_image(url))
-        st.write('')
-        st.button('Continue Game', on_click=continue_game)
+    if start:
+        cols = st.columns([1, 2])
+        with cols[0]:
+            url = 'https://cdn1.iconfinder.com/data/icons/finance-251/64/56-64.png'
+            st.image(images.get_image(url))
+            st.write('')
+            st.button('Start New Game', on_click=new_game)
+        with cols[1]:
+            url = 'https://cdn0.iconfinder.com/data/icons/customicondesignoffice5/64/continue.png'
+            st.image(images.get_image(url))
+            st.write('')
+            st.button('Continue Game', on_click=continue_game)
 
 
 # New Game
--- game.py
+++ game.py
@@ -1,9 +1,7 @@
-
 from design import design
 from stock_data import stocks
 
-
-# setting up basic setup
+# setting up basic page setup
 design.basic_setup()
 
 # At start up of App
