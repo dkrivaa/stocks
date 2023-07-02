47b6b30b858e3dc4b2d9af65693ac7a3e50c5e58
dkrivaa <dannykrivaa@hotmail.com>
2023-07-02 14:42:04 +0300

    Stock game

--- design/design.py
+++ design/design.py
@@ -1,11 +1,9 @@
 import streamlit as st
-import webbrowser
 
 
 from design import images
 
 
-start = True
 
 # BASIC SETUP OF WEB PAGE
 def basic_setup():
@@ -18,23 +16,23 @@ def basic_setup():
 
 # Start page
 def start_page():
-    if start:
-        cols = st.columns([1, 2])
-        with cols[0]:
-            url = 'https://cdn1.iconfinder.com/data/icons/finance-251/64/56-64.png'
-            st.image(images.get_image(url))
-            st.write('')
-            st.button('Start New Game', on_click=new_game)
-        with cols[1]:
-            url = 'https://cdn0.iconfinder.com/data/icons/customicondesignoffice5/64/continue.png'
-            st.image(images.get_image(url))
-            st.write('')
-            st.button('Continue Game', on_click=continue_game)
+    cols = st.columns([1, 2])
+    with cols[0]:
+        url = 'https://cdn1.iconfinder.com/data/icons/finance-251/64/56-64.png'
+        st.image(images.get_image(url))
+        st.write('')
+        st.button('Start New Game', on_click=new_game)
+    with cols[1]:
+        url = 'https://cdn0.iconfinder.com/data/icons/customicondesignoffice5/64/continue.png'
+        st.image(images.get_image(url))
+        st.write('')
+        st.button('Continue Game', on_click=continue_game)
 
 
 # New Game
 def new_game():
-    webbrowser.open('https://trading-stocks.streamlit.app/new_game')
+    st.write('test')
+
 
 # Continue game
 def continue_game():
