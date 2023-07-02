7e699127b0cb91158a213230d4ab07654e608b19
dkrivaa <dannykrivaa@hotmail.com>
2023-07-02 16:51:53 +0300

    Stock game

--- game.py
+++ game.py
@@ -6,10 +6,10 @@ from repo import repo_files
 # setting up basic page setup
 design.basic_setup()
 
-st.write(stocks.get_data())
+base = (stocks.get_data())
 
 # Save base df
-# repo_files.save_new_file(stocks.get_data(), 'test')
+repo_files.save_new_file(base, 'test')
 
 
 
