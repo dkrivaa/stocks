7fb644ba8301887522505a1f58b71071624e2a31
dkrivaa <dannykrivaa@hotmail.com>
2023-07-03 10:22:36 +0300

    Stock game

--- trade/trade.py
+++ trade/trade.py
@@ -1,18 +1,21 @@
+import streamlit as st
+
 from stock_data import stocks
 
-cash = 100000
-cash = "{:,}".format(cash)
 s_data = stocks.get_data()
 
 
-def buy(symbol, amount):
+def buy(symbol, amount):     # symbol in ''
+    cash = 100000
     price = s_data.loc[s_data['symbol'] == symbol, 'price']
-    cost = price * amount
+    cost = int(price * amount)
     if cost <= cash:
         new_stock = {'symbol': symbol, 'amount': amount, 'buy_price': price}
         cash = cash - cost
+    else:
+        st.success('You do NOT have enough cash to make this purchase')
 
-
+buy('AAPL', 10000)
 
 def sell(symbol, amount):
     pass
