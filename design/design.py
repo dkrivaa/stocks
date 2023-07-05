1743a01128997ab6cb4eab88095c0b1cb9fe47da
dkrivaa <dannykrivaa@hotmail.com>
2023-07-05 07:25:26 +0300

    Stock game

--- design/design.py
+++ design/design.py
@@ -5,27 +5,12 @@ from trade import trade
 from design import images
 
 
-# New Game
-def new_game():
-    try:
-        repo_files.del_file('base')
-        # repo_files.del_file('my_stocks')
-    except:
-        pass
-    # Save base df
-    base = (stocks.get_data())
-    # repo_files.save_new_file(base, 'base')
-    cash = 100000
-    return cash
-
-
-
 # BASIC SETUP OF WEB PAGE
 def basic_setup():
     st.set_page_config(page_title='Nasdaq Stock Game', layout='wide')
     st.markdown(f'<span style="color: #18448c; font-size: 32px"><b>NASDAQ STOCK GAME</b></span>'
              , unsafe_allow_html=True)
-    st.button(':red[New Game]', on_click=new_game())
+    st.button(':red[New Game]', on_click=new_game)
     st.markdown('___')
 
     # Sidebar
@@ -38,6 +23,18 @@ def basic_setup():
         st.markdown('___')
 
 
+# New Game
+def new_game():
+    try:
+        repo_files.del_file('base')
+        # repo_files.del_file('my_stocks')
+    except:
+        pass
+    # Save base df
+    base = (stocks.get_data())
+    # repo_files.save_new_file(base, 'base')
+    cash = 100000
+    return cash
 
 
 
--- game.py
+++ game.py
@@ -3,6 +3,7 @@ from design import design
 from stock_data import stocks
 from repo import repo_files
 
+
 # setting up basic page setup
 design.basic_setup()
 
