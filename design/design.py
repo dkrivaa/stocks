1c2f77718e935f32e5c49b799f6c5947622566a1
dkrivaa <dannykrivaa@hotmail.com>
2023-07-05 09:43:35 +0300

    Stock game

--- design/design.py
+++ design/design.py
@@ -25,7 +25,8 @@ def basic_setup():
     # updated stock data
     s_data = stocks.get_data()
 
-    cash = my_cash.loc[0, 0]
+    cash = my_cash.iloc[0, 0]
+    print(type(cash))
 
 
     # Sidebar
