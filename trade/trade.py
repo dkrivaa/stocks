136cb2e4a6d1c15352b29154fc7614a373562ca6
dkrivaa <dannykrivaa@hotmail.com>
2023-07-09 07:47:28 +0300

    Stock game

--- trade/trade.py
+++ trade/trade.py
@@ -14,9 +14,9 @@ def buy(symbol, amount):     # symbol in ''
     my_cash = repo_files.read_file('my_cash')
     # updated stock data
     s_data = stocks.get_data()
-    cash = my_cash
-    price = s_data.loc[s_data['symbol'] == symbol, 'price']
-    cost = int(price * amount)
+    cash = int(my_cash)
+    price = int(s_data.loc[s_data['symbol'] == symbol, 'price'])
+    cost = price * amount
     if cost <= cash:
         new_stock = {'symbol': symbol, 'amount': amount, 'buy_price': price}
         cash = cash - cost
