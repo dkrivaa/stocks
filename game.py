6f6313db722aea3d4143aaa150132a19cea91306
dkrivaa <dannykrivaa@hotmail.com>
2023-07-09 07:43:47 +0300

    Stock game

--- game.py
+++ game.py
@@ -2,13 +2,14 @@ import streamlit as st
 from design import design
 from stock_data import stocks
 from repo import repo_files
+from trade import trade
 
 
 # setting up basic page setup
 design.basic_setup()
 
 
-
+trade.buy('AAPL', 10)
 
 
 
