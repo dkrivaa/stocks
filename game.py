899fa296426f2e8cae36bdb3f6d1137dd243c32a
dkrivaa <dannykrivaa@hotmail.com>
2023-07-17 15:18:08 +0300

    Stock game

--- game.py
+++ game.py
@@ -7,12 +7,10 @@ from trade import trade
 
 # setting up basic page setup
 design.basic_setup()
-st.write("Streamlit version:", st.__version__)
 
 # Save game
 st.sidebar.button('save game', on_click=design.save_game)
 
-st.dataframe(df, column_config={"Website": st.column_config.LinkColumn()})
 
 
 
