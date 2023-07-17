d4c230a566cdd12359ec19e6d8cc39e357222736
dkrivaa <dannykrivaa@hotmail.com>
2023-07-17 19:21:45 +0300

    Stock game

--- design/design.py
+++ design/design.py
@@ -34,7 +34,7 @@ def basic_setup():
         my_symbol = my_stocks['symbol'].unique().tolist()
         for s in my_symbol:
             my_stocks['price'] = int(s_data.loc[s_data['symbol'] == s, 'price'])
-        my_stocks['change'] = (my_stocks['price']/my_stocks['org_price'])
+        my_stocks['change'] = float(my_stocks['price'])/float(my_stocks['org_price'])
         my_stocks['sell'] = None
     st.dataframe(my_stocks, column_config={
         'symbol': st.column_config.Column('Symbol', disabled=True),
