10e97744f1cff92027dfab2079c56f5a781227ee
dkrivaa <dannykrivaa@hotmail.com>
2023-07-18 14:28:09 +0300

    Stock game

--- design/design.py
+++ design/design.py
@@ -62,7 +62,7 @@ def basic_setup():
     for i in range(0, len(exist_stocks)):
         sell_list = []
         if exist_stocks['sell'][i]:
-            sell_list.append(exist_stocks['symbol'])
+            sell_list.append(exist_stocks['symbol'][i])
     st.write(sell_list)
     return sell_list
 
